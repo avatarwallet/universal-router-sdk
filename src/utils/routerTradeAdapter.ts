32b7c2adc353cfad9d5b6a3748cbc4e815d85f48
zf <feng.zeng@innoguild.com>
2024-05-13 18:22:21 +0800

    feat:v0.0.1

--- src/utils/routerTradeAdapter.ts
+++ src/utils/routerTradeAdapter.ts
@@ -1,7 +1,7 @@
-import { MixedRouteSDK, Trade as RouterTrade } from '@uniswap/router-sdk'
-import { Currency, CurrencyAmount, Ether, Token, TradeType } from '@uniswap/sdk-core'
-import { Pair, Route as V2Route } from '@uniswap/v2-sdk'
-import { Pool, Route as V3Route, FeeAmount } from '@uniswap/v3-sdk'
+import { MixedRouteSDK, Trade as RouterTrade } from '@xeiswap/router-sdk'
+import { Currency, CurrencyAmount, Ether, Token, TradeType } from '@xeiswap/sdk-core'
+import { Pair, Route as V2Route } from '@xeiswap/v2-sdk'
+import { Pool, Route as V3Route, FeeAmount } from '@xeiswap/v3-sdk'
 import { BigNumber } from 'ethers'
 import { ETH_ADDRESS, E_ETH_ADDRESS } from './constants'
 
